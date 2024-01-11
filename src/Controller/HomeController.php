78d2b1d686e5343c1e77d5536222b515ffd4c836
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2024-01-11 13:08:56 +0100

    Update Images

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,16 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArmesRepository;
+use App\Repository\DegatsRepository;
 
 class HomeController extends AbstractController {
     #[Route('/', name: 'home')]
-    public function index(CategoriesRepository $categorieRepository, ArmesRepository $armesRepository): Response {
+    public function index(CategoriesRepository $categorieRepository, ArmesRepository $armesRepository, DegatsRepository $degatsRepository): Response {
         $categories = $categorieRepository->findAll();
         $armes = $armesRepository->findAll();
-
+        $degats = $degatsRepository->findAll(); 
         return $this->render('home/index.html.twig', [
             'categories' => $categories,
             'armes' => $armes,
+            'degats' => $degats
         ]);
     }
 }
