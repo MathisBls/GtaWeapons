89573412878957b268068564228373484d7cbbea
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2024-01-11 15:44:06 +0100

    twig & index modif

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,17 +8,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArmesRepository;
 use App\Repository\DegatsRepository;
+use GuzzleHttp\Client;
 
 class HomeController extends AbstractController {
     #[Route('/', name: 'home')]
     public function index(CategoriesRepository $categorieRepository, ArmesRepository $armesRepository, DegatsRepository $degatsRepository): Response {
-        $categories = $categorieRepository->findAll();
-        $armes = $armesRepository->findAll();
-        $degats = $degatsRepository->findAll(); 
+        $client = new Client(['verify' => false]);
+        $response = $client->get('https://localhost:8000/api/categoriess')->getBody()->getContents();
+        $categories = json_decode($response, true);
+        $response = $client->get('https://localhost:8000/api/armess')->getBody()->getContents();
+        $armes = json_decode($response, true);
+        $response = $client->get('https://localhost:8000/api/degatss')->getBody()->getContents();
+        $degats = json_decode($response, true);
         return $this->render('home/index.html.twig', [
-            'categories' => $categories,
-            'armes' => $armes,
-            'degats' => $degats
+            'categories' => $categories['hydra:member'],
+            'armes' => $armes['hydra:member'],
+            'degats' => $degats['hydra:member']
         ]);
     }
 }
